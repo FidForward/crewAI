1b500946bef49cff41a8e813a66cb6e32f97d061
rbatista191 <ricardo.serodio.batista@gmail.com>
2024-10-07 22:32:17 +0200

    updated agents and tasks for claygent

--- claygent/src/claygent/crew.py
+++ claygent/src/claygent/crew.py
@@ -5,7 +5,7 @@ from crewai.project import CrewBase, agent, crew, task
 # from claygent.tools.custom_tool import MyCustomTool
 
 # Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
+from crewai_tools import SerperDevTool
 
 @CrewBase
 class ClaygentCrew():
@@ -15,29 +15,38 @@ class ClaygentCrew():
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+			verbose=True,
+			tools=[SerperDevTool(n_results=3)],
+			max_iter=2
+		)
+
+	# @agent
+	# def reviewer(self) -> Agent:
+	# 	return Agent(
+	# 		config=self.agents_config['reviewer'],
+	# 		verbose=True
+	# 	)
+
+	@task
+	def linkedin_scraper_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['linkedin_scraper'],
+			verbose=True
+		)
+  
+	@task
+	def employee_scraper_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['employee_scraper'],
 			verbose=True
 		)
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
-
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
-
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+	# @task
+	# def reviewer_task(self) -> Task:
+	# 	return Task(
+	# 		config=self.tasks_config['reviewer_task'],
+	# 		output_file='report.md'
+	# 	)
 
 	@crew
 	def crew(self) -> Crew:
@@ -46,6 +55,6 @@ class ClaygentCrew():
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
-			verbose=True,
+   			verbose=True,
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
